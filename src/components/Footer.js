c0f27bef6a59bee8eeb1183f7fa623acc9515271
Alvin <alvin.gikunju@student.moringaschool.com>
2023-05-03 23:59:05 +0300

    updated version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+//import {fontAwesomeIcon} from "@fortawesome/react-fontawesome"
+//import { faHippo} from "@fortawesome/fontawesome-svg-core"
 import '../css/Footer.css'
 function Footer() {
   return (
@@ -9,23 +11,22 @@ function Footer() {
                <p>Let us Give you what you deserve</p>
             </div>
             <div className='column'>
-              <h3>Contacts</h3>
+              <h3>Contacts <div className='underline'><span></span></div></h3>
               <p>Nairobi</p>
               <p>Ngong Road</p>
               <p className='email-id'>MindBenders@gmail.com</p>
               <h4>+254720895727</h4>
             </div>
             <div className='column'>
-              <h3>Links</h3>
+              <h3>Links <div className='underline'><span></span></div></h3>
               <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">About</a></li>
+                <li><a href="home">Home</a></li>
+                <li><a href="sevices">Services</a></li>
+                <li><a href="about">About</a></li>
               </ul>
             </div>
             <div className='column'>
               <div className='social-icons'>
-                <i className='fab fa-facebook-f'></i>
                 <i className='fab fa-twitter'></i>
                 <i className='fab fa-whatsapp'></i>
                 <i className='fab fa-pinterest'></i>
