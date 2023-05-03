6ca946a4089299aa8eab9233b71a1bf0456605d7
jelizasilatei <silateijc90@gmail.com>
2023-05-03 22:54:26 +0300

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar"
+// import home from "./pages/Pricing"
+// import Home from "./Home"
+// import About from "./About"
+// import Services from "./Services"
+// import Contact from "./Contact"
+// import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes> */}
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="mind-benders">
+        Mind-Benders Library
+      </Link>
+      <ul>
+        <CustomLink to="/home">Home</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/services">Services</CustomLink>
+        <CustomLink to="/contact">Contact Us</CustomLink>
+      </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+export default Navbar;
