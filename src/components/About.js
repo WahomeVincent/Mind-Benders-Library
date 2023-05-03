eaf686eb76cbb3b858fd0e757e7058aa75585456
Alvin <alvin.gikunju@student.moringaschool.com>
2023-05-03 20:51:10 +0300

    learn delete

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/About.css'
+function About() {
+    
+  return (
+    <div className='About'>
+        <h2>About</h2>
+        <div className='paragraphs'>
+        <p>MindBenders offers state of the art serrvicing to your daily requirements</p>
+        <p>Some of the services we offer are...</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,39 @@
 import React from 'react'
-
-
+import '../css/Footer.css'
 function Footer() {
   return (
-      <footer className="footer">
-    <div className="row">
-        <div className="tag">About Us</div>
-        <div className="tag">Terms & Conditions</div>
-        <div className="tag">Privacy Policy</div>
-    </div>
-    <div className="socials">
-        <a href="https://www.facebook.com/" >FaceBook</a>
-        <a href="https://twitter.com/" >Twitter</a>
-        <a href="https://www.instagram.com/" >Instagram</a>
-        <a href="https://www.linkedin.com/" >LinkedIn</a>
-    </div>
-    <div className="contact">
-        <div className="title">Contact Us</div>
-        <div className="info">
-            <span>MindBenders@company.com</span>
-            <p>+254720895727</p>
-            <p>0721268493</p>
+    <footer>
+        <div className='row'>
+            <div className='column'>
+               <img src='' alt='' />
+               <p>Let us Give you what you deserve</p>
+            </div>
+            <div className='column'>
+              <h3>Contacts</h3>
+              <p>Nairobi</p>
+              <p>Ngong Road</p>
+              <p className='email-id'>MindBenders@gmail.com</p>
+              <h4>+254720895727</h4>
+            </div>
+            <div className='column'>
+              <h3>Links</h3>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">About</a></li>
+              </ul>
+            </div>
+            <div className='column'>
+              <div className='social-icons'>
+                <i className='fab fa-facebook-f'></i>
+                <i className='fab fa-twitter'></i>
+                <i className='fab fa-whatsapp'></i>
+                <i className='fab fa-pinterest'></i>
+              </div>
+            </div>
         </div>
-    </div>
-    <div className="description">
-        <p id='line'>Let us Give you what you deserve</p>
-        <p>Copyright © 2023</p>
-    </div>
-</footer>
+
+    </footer>
   )
 }
 
